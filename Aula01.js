65ae443c35800c22969ea411831811936333dbd0
Felipe Almeida <falmeida.1305@gmail.com>
2021-06-15 18:48:36 -0300

    Praticando em JavaScript

---
+++ Aula01.js
@@ -0,0 +1,12 @@
+var Pessoas = [
+    {id: 1, nome:"Felipe", idade:27},
+    {id: 2, nome:"Fabricio", idade:27},
+    {id: 3, nome:"Nicolas", idade:19},
+    {id: 4, nome:"Maria", idade:53},
+    {id: 5, nome:"Lourdes", idade: 87}];
+
+    function printarNome() {
+        document.write(Pessoas.toLocaleString)
+    }
+
+    printarNome();
